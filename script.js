18b5936f96d0a2bc62581e0fa4ddbcd2df3c5d33
victorphillips1 <victor.phillips@ocsbstudent.ca>
2021-03-22 13:45:17 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+// sets default values
+let l1 = 0
+let l2 = 0
+let h = 0
+let output = 0
+// detects when button is pressed
+document.getElementById('button').addEventListener('click', solve)
+// calculates the area
+function solve () {
+  l1 = document.getElementById('num1').value
+  l1 = parseInt(l1)
+  l2 = document.getElementById('num2').value
+  l2 = parseInt(l2)
+  h = document.getElementById('num3').value
+  h = parseInt(h)
+  output = l1 + l2
+  output = output * h
+  output = output * 2
+  document.getElementById('output').innerHTML = output
+  if (l1 < 1) {
+    document.getElementById('output').innerHTML = 'error:one or more of your imputed numbers are ether 0 or negative'
+  }
+   if (l2 < 1) {
+    document.getElementById('output').innerHTML = 'error:one or more of your imputed numbers are ether 0 or negative'
+  }
+   if (h < 1) {
+    document.getElementById('output').innerHTML = 'error:one or more of your imputed numbers are ether 0 or negative'
+  }
+}
